878ebc068ee908e2bcf82b4860a7b31f5a065af4
DomenicaRamosJacome <domeramos495@gmail.com>
2024-07-06 16:10:59 -0500

    Add files via upload

---
+++ practicas/P1/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentloaded',function(){
+    var boton = document.getElementById('miBoton');
+    boton.addEventListener('click',cambiarContenido);
+});
+
+function cambiarContenido(){
+    var parrafo=document.getElementById('Parrafo');
+    parrafo.innerHTML="El contenido ha sido cambiado";
+    parrafo.style.color="red"
+}
